688ada74a2cde34f3fa7a552a7e22c5bcfe219e5
Gert Meulyzer <gertm@Gerts-Air.fritz.box>
2024-01-01 22:02:56 +0100

    Converting it to a channel based setup

--- watchandqueue.go
+++ watchandqueue.go
@@ -1,15 +1,23 @@
 package watchandqueue
 
 import (
+	"context"
 	"log"
 	"os"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-func watchForFiles(watchDirectory string, f func() error) {
-	count = 0
+var (
+	count           int
+	monitored_files sync.Map
+	PollInterval    = 3
+)
+
+func watchForIncomingFiles(ctx context.Context, watchDirectory, extension string, c chan<- string) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -25,7 +33,7 @@ func watchForFiles(watchDirectory string, f func() error) {
 	}
 
 	done := make(chan bool)
-	go func() {
+	go func(ctx context.Context) {
 		for {
 			select {
 			case event, ok := <-watcher.Events:
@@ -36,7 +44,7 @@ func watchForFiles(watchDirectory string, f func() error) {
 					if _, ok := monitored_files.Load(event.Name); ok {
 						// already watching this file
 						log.Printf("Already watching %v\n", event.Name)
-						return
+						continue
 					} else {
 						monitored_files.Store(event.Name, true)
 						count++
@@ -46,18 +54,9 @@ func watchForFiles(watchDirectory string, f func() error) {
 							log.Printf("A new file is being written: %v\n", event.Name)
 							err := waitForUploadToFinish(event.Name)
 							if err != nil {
-								// Could not wait for the file correctly. Something must have gone awry.
 								log.Println(err)
 							} else {
-								count -= 1
-								if count <= 0 {
-									log.Println("No more files being written to.")
-									if err := f(); err != nil {
-										log.Println(err)
-									}
-								} else {
-									log.Println("Some files are still being written to...", count)
-								}
+								c <- event.Name
 							}
 						}
 					}()
@@ -67,9 +66,13 @@ func watchForFiles(watchDirectory string, f func() error) {
 					return
 				}
 				log.Println("error:", err)
+			case <-ctx.Done():
+				log.Println("Being cancelled by context.")
+				return
 			}
+
 		}
-	}()
+	}(ctx)
 
 	log.Printf("Watching directory %v for incoming .mkv files.\n", dir)
 	err = watcher.Add(dir)
@@ -77,4 +80,38 @@ func watchForFiles(watchDirectory string, f func() error) {
 		log.Fatal(err)
 	}
 	<-done
+	return nil
+}
+
+// This is a bit of a naive way of checking if the file is done writing.
+// Yet it works quite well in practise for me. Then again, I have quite
+// reliable internet, so that helps. So this can certainly be improved.
+func waitForUploadToFinish(file string) error {
+	var size int64
+	size = 0
+	sameSizeCount := 0
+	log.Printf("Waiting for write operations to stop on %v\n", file)
+	defer func() {
+		count--
+		monitored_files.Delete(file)
+	}()
+	for {
+		time.Sleep(time.Duration(PollInterval) * time.Second)
+		fi, err := os.Stat(file)
+		if err != nil {
+			return err
+		}
+		currentSize := fi.Size()
+		if currentSize > size {
+			size = currentSize
+			sameSizeCount = 0
+			continue
+		}
+		if currentSize == fi.Size() {
+			sameSizeCount += 1
+		}
+		if sameSizeCount == 3 {
+			return nil
+		}
+	}
 }
